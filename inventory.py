f645d924bb307e0db3624333648333335fa3906c
ShammmN <shamnmretc@gmail.com>
2026-03-05 13:25:35 +0000

    added functions and tests 79% cover

---
+++ inventory.py
@@ -0,0 +1,28 @@
+{
+    "items":    [],           # list of item name strings
+    "capacity": 10,          # maximum number of items allowed
+    "locked":   False        # if True, inventory cannot be modified
+}
+
+def add_item(inventory, item):
+    if inventory["locked"] == True:
+        return inventory
+    if item == "":
+        raise ValueError("The item can not be an empty string!")
+    if len(inventory["items"]) >= inventory["capacity"]:
+        raise ValueError("The inventory of items is at capacity!") 
+    inventory["items"].append(item)
+    return inventory 
+
+def remove_item(inventory, item):
+    if inventory["locked"] == True:
+        return inventory
+    if item not in inventory["items"]:
+        raise ValueError("This item is not in the inventory!")
+    inventory["items"].remove(item)
+    return inventory 
+
+def get_item_count(inventory):
+    return len(inventory["items"])
+
+
